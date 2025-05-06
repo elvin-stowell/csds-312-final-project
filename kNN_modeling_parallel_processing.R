8cb5ed94fc021c4e5c32382b10ee47816b245201
elvin-stowell <estowellvt@gmail.com>
2025-05-05 22:49:26 -0400

    Add files via upload

--- kNN_modeling_parallel_processing.R
+++ kNN_modeling_parallel_processing.R
@@ -9,12 +9,12 @@ model_data_50d_noNA <- readRDS("~/PROJECT/RDS_files/model_data_50d_noNA.rds")
 
 # Split data
 set.seed(999)
-data_split <- initial_split(model_data_50d_noNA, prop = 0.98)
+data_split <- initial_split(model_data_50d_noNA, prop = 0.995)
 train_data <- training(data_split)
 test_data <- testing(data_split)
 
 # Sample sizes
-sample_sizes <- c(10000, 25000, 50000, 100000, 200000, 300000, 400000, 500000, 750000, 1338493)
+sample_sizes <- c(10000)
 
 # Set up parallel backend
 n_cores <- parallel::detectCores(logical = FALSE)
@@ -49,12 +49,12 @@ foreach(n = sample_sizes, .packages = c("tidymodels", "kknn", "dplyr")) %dopar% 
   end_time <- Sys.time()
   
   # Save predictions
-  filename <- paste0("~/PROJECT/model_preds/kNN/knn_test_predictions_", format(n, scientific = FALSE), ".rds")
+  filename <- paste0("~/PROJECT/model_preds/kNN_old/knn_test_predictions_", format(n, scientific = FALSE), ".rds")
   saveRDS(knn_test_preds, filename)
   
   # Save timing info
   timing_info <- paste("Sample size:", n, "\nElapsed time:", round(difftime(end_time, start_time, units = "mins"), 2), "minutes")
-  log_filename <- paste0("~/PROJECT/runtime_analysis/knn_model_log_", format(n, scientific = FALSE), ".txt")
+  log_filename <- paste0("~/PROJECT/runtime_analysis/kNN_old/knn_model_log_", format(n, scientific = FALSE), ".txt")
   writeLines(timing_info, log_filename)
 }
 
